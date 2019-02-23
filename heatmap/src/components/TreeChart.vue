af96531c7ae7cc4251c19a48dbc34631e1100f8a
arman <feyziarman@gmail.com>
2019-02-24 01:32:01 +0330

    remove unless code

--- heatmap/src/components/TreeChart.vue
+++ heatmap/src/components/TreeChart.vue
@@ -36,7 +36,6 @@ export default {
     });
 
     // console.log(NEW_DATA);
-    
     let groubedByExchange = groupBy(NEW_DATA, 'ex');
 
     chart.padding(0,0,0,0);
@@ -47,92 +46,48 @@ export default {
 
     let data = groubedByExchange;
 
-    // only one level visible initially
-    chart.maxLevels = 2;
-    chart.zoomable = true;
-    chart.zoomToChartDataItem()
-
     chart.dataFields.value = "value";
     chart.dataFields.name = "name";
     chart.dataFields.children = "children";
     chart.dataFields.color = "cc";
 
-    // chart.navigationBar = new am4charts.NavigationBar();
-    // chart.homeText = "TOP";
-
-    // chart.zoomAxes = 20000;
-    // chart.zoomable = true;
-    // chart.maxZoomFactor = 100;
-    // chart.layoutAlgorithm = chart.binaryTree;
-
-    // chart.chartContainer.wheelable = true;
+    chart.navigationBar = new am4charts.NavigationBar();
+    chart.homeText = "خانه";
+    chart.zoomable = true;
+    chart.maxZoomFactor = 0;
+    chart.layoutAlgorithm = chart.binaryTree;
+    chart.chartContainer.wheelable = true;
 
     // LEVEL 1
     let level1 = chart.seriesTemplates.create("0");
-    level1.strokeWidth = 9;
+    level1.strokeWidth = 11;
 
-// by default only current level series bullets are visible, but as we need brand bullets to be visible all the time, we modify it's hidden state
-level1.bulletsContainer.hiddenState.properties.opacity = 1;
-level1.bulletsContainer.hiddenState.properties.visible = true;
-// create hover state
-var hoverState = level1.columns.template.states.create("hover");
-
-// darken
-hoverState.adapter.add("fill", (fill, target) => {
-  return am4core.colors.brighten(fill, -0.2);
-});
-
-
-    let level1_column = level1.columns.template;
-    level1_column.stroke = am4core.color("red");
-
-    level1_column.fillOpacity = 1;
-    level1_column.fill = "#88ff22";
-    level1_column.strokeOpacity = 1;
-    level1_column.strokeWidth = 15;
+    level1.columns.stroke = am4core.color("red");
+    level1.columns.fillOpacity = 1;
+    level1.columns.fill = "#88ff22";
+    level1.columns.strokeOpacity = 1;
 
     let level1_bullet = level1.bullets.push(new am4charts.LabelBullet());
-    level1_bullet.locationY = 0.5;
+    level1_bullet.locationY = 1;
     level1_bullet.locationX = 0.5;
-    level1_bullet.label.text = "[bold font-size:5pc;]{name}\n[font-size: 8pc;]{percent.formatNumber('#.00')}";
-    level1_bullet.fontSize = "1.3pc";
-    level1_bullet.fontFamily = "Vazir Code";
-    level1_bullet.label.fill = am4core.color("#fff");
+    level1_bullet.label.text = "[bold font-size:3pc;]{name}";
+    level1_bullet.label.fill = am4core.color("#f09");
     
-
     // LEVEL 2
     let level2 = chart.seriesTemplates.create("1");
-    let level2_column = level2.columns.template;
-    level2_column.stroke = am4core.color("#fff");
-    level2_column.fillOpacity = 1;
-    level2_column.strokeWidth = 1;
-    level2_column.strokeOpacity = 1;
-    let level2_bullet = level2.bullets.push(new am4charts.LabelBullet());
-    level2_bullet.locationY = 0.5;
-    level2_bullet.locationX = 0.5;
-    level2_bullet.label.text = "[bold font-size:1pc;]{shortname}\n[font-size: 1.8pc;]{percent.formatNumber('##.##')}";
-    // chart.numberFormatter.numberFormat = "##.##";
-    // level2_bullet.label.text = "[bold font-size: 1.4pc; #fff]{shortname}[/]\n[font-size: 0.9pc]% {percent}[/]";
-    // level2_bullet.fontSize = "1pc";
-    // level2_bullet.fontFamily = "Vazir Code";
-    level2_bullet.label.fill = am4core.color("#fff");
-    level2_column.tooltipText = "Value: [bold]{value}[/]";
-    level2_column.tooltipText = `[bold]{name}[/]\n
+    level2.columns.template.fillOpacity = 1;
+    level2.columns.stroke = am4core.color("#fff");
+    level2.columns.tooltipText = `[bold]{name}[/]\n
                                   قیمت سهم: [bold]{value}[/]\n
                                   حجم سهام معامله شده : [bold]{volume}[/]\n
                                   درصد: [bold]{percent}[/]`;
     
-    // level1_column.zoomable = true;
-    // level2_column.zoomable = true;
+    let level2_bullet = level2.bullets.push(new am4charts.LabelBullet());
+    level2_bullet.locationY = 0.5;
+    level2_bullet.locationX = 0.5;
+    level2_bullet.label.text = "[bold font-size: 1.4pc; #fff]{shortname}[/]\n[font-size: 0.9pc]% {percent.formatNumber(##.##)}[/]";
+    level2_bullet.label.fill = am4core.color("#fff");
     
-    // level1_column.minZoomCount = 20000;
-    // level2_column.minZoomCount = 20000;
-
-
-    // // level2_column.margin(0,21,0,21);
-    // level2_column.padding(51,21,51,21);
-    // // level2_column
-
     chart.data = data;
 
     this.chart = chart;
